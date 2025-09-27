8a2fd2d57f119a7683c0f4f91a5a777e981d9f58
Raielly Souza <souzaraielly@gmail.com>
2025-09-27 12:16:13 -0300

    First Commit

---
+++ src/main/java/com/example/pdv_galeteria/PdvGaleteriaApplication.java
@@ -0,0 +1,13 @@
+package com.example.pdv_galeteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdvGaleteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdvGaleteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/pdv_galeteria/PdvGaleteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pdv_galeteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvGaleteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
