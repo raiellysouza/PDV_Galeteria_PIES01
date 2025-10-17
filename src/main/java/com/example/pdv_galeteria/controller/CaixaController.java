5f571faaef168745019815d18b04558495603b6f
Raielly Souza <souzaraielly@gmail.com>
2025-10-16 22:36:02 -0300

    Cadastro de produtos e configuracao BD

---
+++ src/main/java/com/example/pdv_galeteria/controller/CaixaController.java
@@ -0,0 +1,5 @@
+package com.example.pdv_galeteria.controller;
+
+public class CaixaController {
+    
+}
---
+++ src/main/java/com/example/pdv_galeteria/model/Produto.java
@@ -0,0 +1,23 @@
+package com.example.pdv_galeteria.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private Double preco;
+
+    @Column(nullable = false)
+    private Integer quantidade;
+}
