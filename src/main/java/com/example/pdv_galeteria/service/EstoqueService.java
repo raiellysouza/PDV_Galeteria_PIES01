fc0280f7b69880e678525808e5c9db2c5c57f9de
Levi Gomes <97966218+levigtri@users.noreply.github.com>
2025-12-12 00:37:47 -0300

    Add getters and setters to RemocaoRequest class

--- src/main/java/com/example/pdv_galeteria/service/EstoqueService.java
+++ src/main/java/com/example/pdv_galeteria/service/EstoqueService.java
@@ -40,6 +40,22 @@ public class EstoqueService {
             this.produtoId = produtoId;
             this.quantidade = quantidade;
         }
+        
+        public Long getProdutoId() {
+            return produtoId;
+        }
+
+        public void setProdutoId(Long produtoId) {
+            this.produtoId = produtoId;
+        }
+
+        public Integer getQuantidade() {
+            return quantidade;
+        }
+
+        public void setQuantidade(Integer quantidade) {
+            this.quantidade = quantidade;
+        }
     }
 
     public void removerDoEstoque(Long produtoId, Integer quantidade) {
