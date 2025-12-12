df74a98e7165795242f69bf65c032b5b83487934
Levi Gomes <97966218+levigtri@users.noreply.github.com>
2025-12-12 00:39:41 -0300

    Add EntradaEstoqueRequest import to ProdutoService

--- src/main/java/com/example/pdv_galeteria/service/ProdutoService.java
+++ src/main/java/com/example/pdv_galeteria/service/ProdutoService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 import java.util.Optional;
+import com.example.pdv_galeteria.dto.EntradaEstoqueRequest;
 
 @Service
 public class ProdutoService {
